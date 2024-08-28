9d5b8e282ac4c242d5a05ff1e9ea50e40df8ce2d
Wiktor Kycia <wiktor.kycia@gmail.com>
2024-08-28 17:25:22 +0200

    come back to using prompt again

--- main.ts
+++ main.ts
@@ -16,25 +16,18 @@ class Game {
         this.players = []
     }
 }
-import * as readline from 'readline';
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+const prompt = require('prompt-sync')({sigint: true});
 console.clear();
 let flag:boolean = true;
 let number_of_players:number;
 do {
-    rl.question("How many players will play the game?", (answer) => {
-        try {
-            number_of_players = parseInt(answer);
-            flag = false;
-        } catch(e) {
-            console.log(e.message);
-        }
-        rl.close();
-    })
+    let answer:string = prompt("How many players will play the game?");
+    try {
+        number_of_players = parseInt(answer);
+        flag = false;
+    } catch (e) {
+        console.log(e);
+    }
 } while (flag);
 console.log(number_of_players);
 const game:Game = new Game();
